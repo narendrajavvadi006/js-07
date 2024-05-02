60e27d3e1a540d123552f6a186d9e1b7d87988ed
narendrajavvaji006 <narendrajavvadi006@gmail.com>
2024-05-02 09:20:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('narendra')
+var  f=()=> {
+  var x=100
+}
+f()
+var g = [1, 2, 3]
+
+console.log(Array.isArray(g))
+var h = 'narendra'
+console.log(h, typeof h)
+var age = 40
+console.log(age,typeof age)
+// console.log('value :'h, 'dataType:'typeof h)
+document.writeln('narendra Javvadi')
+var m = [1, 2, 3]
+console.log(m, typeof m, Array.isArray(m))
